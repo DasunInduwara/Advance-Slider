ccdf8c03f87c6b763cd7d49a13b8d4cd7e24756b
Dasun Induwara Perera <dasun@Dasuns-Mac-mini.local>
2023-06-06 23:22:23 +0530

    custom thumb added

--- lib/AdvanceSlider.dart
+++ lib/AdvanceSlider.dart
@@ -51,11 +51,17 @@ class RhombusSlider extends StatefulWidget {
       {required this.min,
       required this.max,
       required this.divisions,
+      required this.customThumb,
+      required this.activeTrackColor,
+      required this.inactiveTrackColor,
       required this.onChanged});
 
   double min;
   double max;
   int divisions;
+  bool customThumb;
+  Color activeTrackColor;
+  Color inactiveTrackColor;
   final ValueChanged<double> onChanged;
 
   @override
@@ -76,17 +82,24 @@ class _RhombusSliderState extends State<RhombusSlider> {
   @override
   Widget build(BuildContext context) {
     return SliderTheme(
-      data: SliderThemeData(
-        trackHeight: 4.0,
-        activeTrackColor: Colors.blue,
-        inactiveTrackColor: Colors.grey.withOpacity(0.3),
-        thumbShape: _CustomThumbShape(),
-        overlayShape: const RoundSliderOverlayShape(overlayRadius: 14.0),
-      ),
+      data: widget.customThumb
+          ? SliderThemeData(
+              trackHeight: 4.0,
+              activeTrackColor: Colors.blue,
+              inactiveTrackColor: Colors.grey.withOpacity(0.3),
+              thumbShape: _CustomRhombusThumbShape(),
+              overlayShape: const RoundSliderOverlayShape(overlayRadius: 14.0),
+            )
+          : SliderThemeData(
+              trackHeight: 4.0,
+              activeTrackColor: Colors.blue,
+              inactiveTrackColor: Colors.grey.withOpacity(0.3),
+              overlayShape: const RoundSliderOverlayShape(overlayRadius: 14.0),
+            ),
       child: Slider(
         value: _value,
-        min: widget.min ?? 0.0,
-        max: widget.max ?? 100.0,
+        min: widget.min,
+        max: widget.max,
         divisions: widget.divisions,
         label: _value.round().toString(),
         onChanged: updateSliderValue,
@@ -95,7 +108,7 @@ class _RhombusSliderState extends State<RhombusSlider> {
   }
 }
 
-class _CustomThumbShape extends SliderComponentShape {
+class _CustomRhombusThumbShape extends SliderComponentShape {
   @override
   Size getPreferredSize(bool isEnabled, bool isDiscrete) {
     return const Size.fromRadius(14.0);
@@ -141,27 +154,29 @@ class _CustomThumbShape extends SliderComponentShape {
 
 // main widget...
 class AdvanceSlider extends StatefulWidget {
-  AdvanceSlider(
-      {required this.min,
-      required this.max,
-      // this.divisions = 100,
-      this.divderCount= 5,
-
-      this.onChanged = _defaultOnChanged,});
+  AdvanceSlider({
+    required this.min,
+    required this.max,
+    this.divderCount = 5,
+    this.divders = true,
+    this.customIcon,
+    this.customThumb = true,
+    this.activeTrackColor = Colors.blue,
+    this.inactiveTrackColor = const Color.fromARGB(255, 207, 207, 207),
+    this.onChanged = _defaultOnChanged,
+  });
 
   double min;
   double max;
-  // int divisions;
-  int divderCount ;
-  bool divders = true;
+  int divderCount;
+  bool divders;
+  Icon? customIcon;
+  bool customThumb;
+  Color activeTrackColor;
+  Color inactiveTrackColor;
   final ValueChanged<double> onChanged;
 
-
-    static void _defaultOnChanged(double value) {
-    // Default implementation for onChanged
-    // You can replace this with your desired logic or leave it empty
-    print('Slider value changed: $value');
-  }
+  static void _defaultOnChanged(double value) {}
 
   @override
   State<AdvanceSlider> createState() => _AdvanceSliderState();
@@ -174,7 +189,7 @@ class _AdvanceSliderState extends State<AdvanceSlider> {
     setState(() {
       _value = value;
     });
-    widget.onChanged!(
+    widget.onChanged(
         value); // Call the onChanged callback provided by the parent widget
   }
 
@@ -183,24 +198,29 @@ class _AdvanceSliderState extends State<AdvanceSlider> {
     return Stack(
       children: [
         Visibility(
-          visible: widget.divders,
-          child: Positioned.fill(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: List.generate(
-                  widget.divderCount,
-                  (index) =>
-                      RhombusWidget(width: 12, height: 12, color: Colors.blue)),
-            ),
-          ),
-        ))
-        ,
+            visible: widget.divders,
+            child: Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: widget.customIcon == null
+                      ? List.generate(
+                          widget.divderCount,
+                          (index) => RhombusWidget(
+                              width: 12, height: 12, color: Colors.blue))
+                      : List.generate(
+                          widget.divderCount, (index) => widget.customIcon!),
+                ),
+              ),
+            )),
         RhombusSlider(
-          divisions: double.parse(widget.max.toString()).toInt() ,
+          divisions: double.parse(widget.max.toString()).toInt(),
           max: widget.max,
           min: widget.min,
+          customThumb: widget.customThumb,
+          activeTrackColor: widget.activeTrackColor,
+          inactiveTrackColor: widget.inactiveTrackColor,
           onChanged: updateSliderValue,
         ),
       ],
